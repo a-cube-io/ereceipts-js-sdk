a0632f92ba755358f4bae0b40802f0dd4f2c1f1a
Anders-planck <anders.jipwouo@gmail.com>
2026-02-09 15:34:15 +0100

    rename all point-of-sale to pems endpoints

--- src/infrastructure/driven/api/point-of-sale.repository.impl.ts
+++ src/infrastructure/driven/api/point-of-sale.repository.impl.ts
@@ -18,14 +18,12 @@ export class PointOfSaleRepositoryImpl implements IPointOfSaleRepository {
   constructor(private readonly http: IHttpPort) {}
 
   async findById(serialNumber: string): Promise<PointOfSaleDetailed> {
-    const response = await this.http.get<PointOfSaleDetailedApiOutput>(
-      `/mf1/point-of-sales/${serialNumber}`
-    );
+    const response = await this.http.get<PointOfSaleDetailedApiOutput>(`/mf1/pems/${serialNumber}`);
     return PointOfSaleMapper.fromDetailedApiOutput(response.data);
   }
 
   async findAll(params?: PointOfSaleListParams): Promise<Page<PointOfSale>> {
-    const response = await this.http.get<Page<PointOfSaleApiOutput>>('/mf1/point-of-sales', {
+    const response = await this.http.get<Page<PointOfSaleApiOutput>>('/mf1/pems', {
       params: {
         status: params?.status,
         page: params?.page,
@@ -37,19 +35,19 @@ export class PointOfSaleRepositoryImpl implements IPointOfSaleRepository {
 
   async activate(serialNumber: string, input: ActivationRequest): Promise<void> {
     const apiInput = PointOfSaleMapper.toActivationApiInput(input);
-    await this.http.post(`/mf1/point-of-sales/${serialNumber}/activation`, apiInput);
+    await this.http.post(`/mf1/pems/${serialNumber}/activation`, apiInput);
   }
 
   async closeJournal(serialNumber: string): Promise<void> {
-    await this.http.post(`/mf1/point-of-sales/${serialNumber}/close`);
+    await this.http.post(`/mf1/pems/${serialNumber}/close`);
   }
 
   async createInactivity(serialNumber: string): Promise<void> {
-    await this.http.post(`/mf1/point-of-sales/${serialNumber}/inactivity`);
+    await this.http.post(`/mf1/pems/${serialNumber}/inactivity`);
   }
 
   async communicateOffline(serialNumber: string, input: PEMStatusOfflineRequest): Promise<void> {
     const apiInput = PointOfSaleMapper.toOfflineApiInput(input);
-    await this.http.post(`/mf1/point-of-sales/${serialNumber}/communicate-offline`, apiInput);
+    await this.http.post(`/mf1/pems/${serialNumber}/communicate-offline`, apiInput);
   }
 }
